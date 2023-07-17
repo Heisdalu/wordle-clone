d5daf93e2c18cf236c3ca24353102a55f13be0a7
divine <divineobi07@gmail.com>
2023-07-17 22:53:12 +0100

    add animation on tiles

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,15 @@ import Keyboard from "./components/KeyBoard/Keyboard";
 import TilesMain from "./components/TilesContainer/TilesMain";
 
 const App = () => {
-  
+  const before = 1689548400000;
+  const present = 1689634800000;
+  const presentDate = new Date();
+  const day = presentDate.getDate();
+  const month = presentDate.getMonth();
+  const year = presentDate.getFullYear();
+  // console.log(day, month, year);
+
+  // console.log((present - before) / (1000 * 60 * 60 * 24));
   return (
     <div className="app">
       <Header />
